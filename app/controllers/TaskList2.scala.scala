512a8ca5140cc1ffff85d408b7032dd03a1a18a2
bw <2846131442@qq.com>
2022-07-28 12:31:54 +0800

    feat(All): Web version 2. (End)
    A revolution of web code which comes to the version 2.
    This is the final update of this version for now,
    though we still have some flaws here.

--- app/controllers/TaskList2.scala.scala
+++ app/controllers/TaskList2.scala.scala
@@ -4,16 +4,91 @@ import javax.inject.Inject
 import play.api.mvc.ControllerComponents
 import play.api.mvc.AbstractController
 import javax.inject._
+import models.TaskListInMemoryModel
 
 @Singleton
-class TaskList2@Inject()(cc:ControllerComponents) extends AbstractController(cc){
+class TaskList2 @Inject() (cc: ControllerComponents)
+    extends AbstractController(cc) {
 
-  def index = Action{
+  def index = Action { implicit request =>
+    // if(request.session.get("username").isEmpty)
+
+    // else
+    //   Redirect(routes.TaskList2.taskListContent)
     Ok(views.html.indexWeb2())
   }
 
-  def login = Action{
+  def login = Action {
     Ok(views.html.loginWeb2())
+  }
+
+  def logout = Action {
+    Redirect(routes.TaskList2.index).withNewSession
+  }
+
+  def registerContent = Action {
+    Ok(views.html.registerWeb2())
+  }
+
+  def taskListContent = Action { implicit request =>
+    val usernameOption = request.session.get("username")
+    usernameOption
+      .map { username =>
+        Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username)))
+      }
+      .getOrElse(Redirect(routes.TaskList2.login))
+  }
+
+  def validateUser = Action {
+    implicit request =>
+      val postVals = request.body.asFormUrlEncoded
+      println(postVals.get("csrfToken").head)
+      postVals
+        .map { argsMap =>
+          val username = argsMap("username").head
+          val password = argsMap("password").head
+          if (TaskListInMemoryModel.validateUser(username, password)) {
+            Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username)))
+              .withSession("username" -> username)
+          } else {
+            Ok(views.html.loginWeb2())
+          }
+        }
+        .getOrElse(
+          Ok(views.html.loginWeb2())
+        )
+  }
+
+  def createUser(username: String, password: String) = Action {
+    implicit request =>
+      if (TaskListInMemoryModel.createUser(username, password)) {
+        Ok(views.html.taskList2(TaskListInMemoryModel.getTasks(username)))
+          .withSession("username" -> username)
+      } else {
+        Ok("false")
+      }
+  }
+
+  def deleteTask(index: Int) = Action { implicit request =>
+    val usernameOption = request.session.get("username")
+    usernameOption
+      .map { username =>
+        if (TaskListInMemoryModel.deleteTask(username, index))
+          Ok("Success!")
+        else
+          Ok("something went wrong")
+      }
+      .getOrElse(Ok("Something went wrong!"))
+  }
+
+  def addTask(task: String) = Action { implicit request =>
+    val usernameOption = request.session.get("username")
+    usernameOption
+      .map { username =>
+        TaskListInMemoryModel.addTasks(username, "", Seq[String](task))
+        Ok("true")
+      }
+      .getOrElse(Ok("False"))
   }
 
 }
--- public/javascripts/indexWeb2.js
+++ public/javascripts/indexWeb2.js
@@ -1,5 +1,84 @@
-$("#buttonLogin").click(function(){
-    $("#contents").load("/login",function(){
-        $("#buttonLogin").hide();
+
+const csrfToken = $("#csrfToken").val();
+
+
+function loadLoginPage(){
+    $("#contents").load("/web2/login", function (responseText, textStatus, XMLHttpRequest) {
+        console.log("loginPage load successfully")
+        if (textStatus == "success") {
+            $("#buttonLogin").hide();
+        }
     });
-});
+}
+
+function login() {
+    const username = $("#login-username").val();
+    const password = $("#login-password").val();
+    $.post(
+        "/web2/validate" ,
+        {username,password,csrfToken},
+        data=>{
+            $("#contents").html(data);
+        }
+        );
+}
+
+function logout(){
+    $("#contents").load("/web2/logout");
+}
+
+function register() {
+    const username = $("#register-username").val();
+    const password = $("#register-password").val();
+    // $("#contents").load("/web2/createUser?username=" + username + "&password=" + password);
+    console.log("Ok into function");
+    $.get("/web2/createUser?username=" + username + "&password=" + password, function (data) {
+        console.log("Ok into huidiao function");
+        console.log(data);
+        if (data == "false") {
+            $("#register-username").val("s");
+            $("#register-password").val("s");
+        }
+        else {
+            $("#contents").html(data);
+        }
+
+    });
+}
+
+function registerContent() {
+    $("#register-div").load("/web2/registerContent");
+}
+
+function createUser() {
+    const username = $("#login-username").val();
+    const password = $("#login-password").val();
+    console.log("Ok into function");
+    $.get("/web2/validate?username=" + username + "&password=" + password, function (data) {
+        console.log("Ok into huidiao function");
+        console.log(data);
+
+        if (data.innerHTML == "false") {
+            $("#register-username").val("123");
+        }
+        else {
+            $("#contents").html(XMLHttpRequest.responseXML);
+        }
+
+    });
+}
+
+function taskListReload() {
+    $("#contents").load("/web2/taskListContent")
+}
+
+function deleteTask(index) {
+    $.get("/web2/deleteTask?index=" + index);
+    taskListReload();
+}
+
+function addTask() {
+    const task = $("#addTask").val()
+    $.get("/web2/addTask?task=" + encodeURIComponent(task))
+    taskListReload();
+}
